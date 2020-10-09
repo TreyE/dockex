7226c34504a6514fbc2a21eacdd0b6632cf632c4
Trey Evans <tevans@justplainawful.lan>
2020-10-09 10:33:46 -0400

    Use newer dmg format.

--- script/build_dmg.sh
+++ script/build_dmg.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-hdiutil create -volname Dockex -srcfolder _build/prod/app -ov -format UDZO _build/prod/Dockex.dmg
+hdiutil create -volname Dockex -srcfolder _build/prod/app -ov -format UDBZ _build/prod/Dockex.dmg
